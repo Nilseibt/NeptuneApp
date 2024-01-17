bc91bc6409ec36d0552a857ce1dfdc69897c66fc
Nilseibt <151017758+Nilseibt@users.noreply.github.com>
2024-01-17 17:18:02 +0100

    test commit nils

--- app/src/main/java/com/example/neptune/MainActivity.kt
+++ app/src/main/java/com/example/neptune/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android simon")
+                    Greeting("Android simon nils")
                 }
             }
         }
