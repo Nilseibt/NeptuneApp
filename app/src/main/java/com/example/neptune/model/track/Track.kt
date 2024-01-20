fc850223c9573ae8d46047bedfbf64b02eadd19b
Martin <martinxscheueuermann@online.de>
2024-01-20 11:53:19 +0100

    Added Track class

---
+++ app/src/main/java/com/example/neptune/model/track/Track.kt
@@ -0,0 +1,20 @@
+package com.example.neptune.model.track
+import  java.util.Date
+class Track (val id: Int,
+             val name: String,
+             val artists: List<String>,
+             val genres: List<String>,
+             val imageUrl: String,
+             var timeStamp:Date,
+             var upvote: Int,
+             var isUpvoted : Boolean) {
+    fun addUpvote(){
+        upvote += 1
+    }
+    fun removeUpvote(){
+        upvote -= 1
+    }
+    fun setUpvote(upvote: Int){
+        this.upvote = upvote
+    }
+}
