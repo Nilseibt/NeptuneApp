3f75570198125d7e6e52eb2f467eb13085338910
Martin <martinxscheueuermann@online.de>
2024-01-20 15:26:21 +0100

    Added TrackList class

---
+++ app/src/main/java/com/example/neptune/model/track/TrackList.kt
@@ -0,0 +1,27 @@
+package com.example.neptune.model.track
+
+class TrackList (val tracks: MutableList<Track>){
+
+    fun removeTrack(index: int){
+            tracks.removeAt(index)
+    }
+    fun addTrack(track: Track){
+        tracks.add(track)
+    }
+    fun containsTrack(track:Track): Boolean{
+        for (item in tracks){
+            if (item.id == track.id){
+                return true
+            }
+        }
+        return false
+    }
+    fun popFirstTrack(): Track{
+        var output  = tracks.first()
+        tracks.removeAt(0);
+        return output;
+    }
+    fun isEmpty(): Boolean{
+        return tracks.isEmpty()
+    }
+}
