22f370b5643eec1dfef8e4e847030229e87d432e
Martin <martinxscheueuermann@online.de>
2024-01-22 12:30:51 +0100

    Added VoteList

---
+++ app/src/main/java/com/example/neptune/model/track/src/VoteList.kt
@@ -0,0 +1,14 @@
+package com.example.neptune.model.track.src
+
+class VoteList(tracks: MutableList<Track>): PlayList(tracks) {
+
+    fun upvoteTrack(index: Int){
+        tracks[index].addUpvote()
+    }
+    fun removeUpvoteFromTrack(index: Int){
+        tracks[index].removeUpvote()
+    }
+    fun sortByUpvote(){
+        tracks.sortBy { track ->  track.upvote }
+    }
+}
