8fe1080d6a1b9cb49aaaea3c757194ee7259ae8f
simonschtz <78378417+simonschtz@users.noreply.github.com>
2024-01-24 18:47:28 +0100

    startView: centered buttons and added space in between them

--- app/src/main/java/com/example/neptune/ui/views/startView/StartView.kt
+++ app/src/main/java/com/example/neptune/ui/views/startView/StartView.kt
@@ -1,17 +1,24 @@
 package com.example.neptune.ui.views.startView
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.neptune.NeptuneApp
 import com.example.neptune.ui.views.util.viewModelFactory
-
 @Composable
 fun StartView(navController: NavController) {
 
@@ -27,16 +34,24 @@ fun StartView(navController: NavController) {
         startViewModel.onBack(navController)
     }
 
-    Column {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Button(onClick = { startViewModel.onJoinSession(navController) }) {
             Text(text = "Session beitreten")
         }
+        Spacer(modifier = Modifier.height(32.dp))
         Button(
             onClick = { startViewModel.onCreateSession(navController) },
             enabled = startViewModel.createSessionPossible()
         ) {
             Text(text = "Session erstellen")
         }
+        Spacer(modifier = Modifier.height(32.dp))
         Button(onClick = { startViewModel.onToggleConnectedToSpotify() }) {
             Text(text = startViewModel.getSpotifyButtonText())
         }
