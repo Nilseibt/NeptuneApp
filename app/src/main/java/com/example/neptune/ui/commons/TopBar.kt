6ec7d57625312c807f716c4b28fc8d543629a829
hash-shark <schneider-stephan1@web.de>
2024-01-25 10:46:26 +0100

    Top Bar

--- app/src/main/java/com/example/neptune/ui/commons/TopBar.kt
+++ app/src/main/java/com/example/neptune/ui/commons/TopBar.kt
@@ -1,12 +1,15 @@
 package com.example.neptune.ui.commons
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -57,5 +61,37 @@ fun TopBar(onBack: () -> Unit) {
         ) {
 
         }
+    }
+}
+
+@Composable
+fun SessionInfoBar(onStatistics: () -> Unit, onSessionInfo: () -> Unit) {
+
+}
+
+@Preview(name = "SessionInfoBar Preview")
+@Composable
+fun SessionInfoBarPreview() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.background))
+
+    Row(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.secondary)
+            .fillMaxWidth()
+            .height(20.dp)
+    ) {
+
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.weight(1f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Info,
+                contentDescription = "info"
+            )
+        }
+
     }
 }
--- app/src/main/java/com/example/neptune/ui/views/voteView/VoteView.kt
+++ app/src/main/java/com/example/neptune/ui/views/voteView/VoteView.kt
@@ -4,33 +4,18 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.neptune.ui.commons.TopBar
 import com.example.neptune.ui.commons.TrackListComposable
 import com.example.neptune.ui.commons.TrackListType
 import com.example.neptune.ui.views.util.viewModelFactory
@@ -101,42 +86,5 @@ fun VoteViewPreview() {
         .fillMaxSize()
         .background(color = MaterialTheme.colorScheme.background))
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(60.dp)
-            .background(color = MaterialTheme.colorScheme.primary)
-    ) {
-        IconButton(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .weight(1f)
-                .align(alignment = Alignment.CenterVertically)
-        ) {
-            Icon(
-                imageVector = Icons.Filled.ArrowBack,
-                contentDescription = "Back"
-            )
-        }
-
-        Text(
-            text = "Neptune",
-            color = MaterialTheme.colorScheme.onPrimary,
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier
-                .weight(4f)
-                .padding(1.dp)
-                .align(alignment = Alignment.CenterVertically),
-            textAlign = TextAlign.Center
-        )
-
-        Button(
-            onClick = { },
-            modifier = Modifier
-                .weight(1f)
-                .align(alignment = Alignment.CenterVertically)
-        ) {
-
-        }
-    }
+    TopBar { }
 }
