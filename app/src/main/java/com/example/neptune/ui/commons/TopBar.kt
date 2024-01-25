4dd874675f88684f7d51c4e9b8b9335bb88cc335
hash-shark <schneider-stephan1@web.de>
2024-01-25 10:00:07 +0100

    Top Bar

--- app/src/main/java/com/example/neptune/ui/commons/TopBar.kt
+++ app/src/main/java/com/example/neptune/ui/commons/TopBar.kt
@@ -1,11 +1,7 @@
 package com.example.neptune.ui.commons
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,10 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun TopBar(onBack: () -> Unit) {
