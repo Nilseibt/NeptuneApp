b0c9266da212524111338a4f1598e1e3355ae2a6
Nilseibt <151017758+Nilseibt@users.noreply.github.com>
2024-01-26 23:40:25 +0100

    tried to write the spotify connector but failed for now, will continue much later

--- app/src/main/java/com/example/neptune/data/model/session/SessionBuilder.kt
+++ app/src/main/java/com/example/neptune/data/model/session/SessionBuilder.kt
@@ -67,12 +67,15 @@ class SessionBuilder {
         return matchingGenres.subList(0, min(matchingGenres.size, 20))
     }
 
+    // let some other method (maybe in the model) create the host backend connector and
+    // make the backend call and then retrieve the full session provided the id and timestamp
+    // from the backend
     fun createSession(sessionId: Int, sessionTimestamp: Int): Session {
         return when(sessionType){
             SessionType.GENERAL -> Session(sessionId, sessionTimestamp, trackCooldown)
             SessionType.ARTIST, SessionType.GENRE -> ArtistSession(sessionId, sessionTimestamp, trackCooldown, selectedEntities)
             SessionType.PLAYLIST -> PlaylistSession(sessionId, sessionTimestamp, trackCooldown, PlayList())
-            //TODO make an acutal playlist in the playlist session
+            //TODO make an actual playlist in the playlist session
         }
     }
 
--- app/src/main/java/com/example/neptune/data/model/streamingConnector/spotifyConnector/SpotifyConnector.kt
+++ app/src/main/java/com/example/neptune/data/model/streamingConnector/spotifyConnector/SpotifyConnector.kt
@@ -1,17 +1,69 @@
 package com.example.neptune.data.model.streamingConnector.spotifyConnector
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
 import com.example.neptune.data.model.streamingConnector.StreamingConnector
+import com.example.neptune.data.model.track.src.Track
 import com.example.neptune.data.model.track.src.TrackList
+import org.json.JSONObject
+import java.net.URLEncoder
 
 open class SpotifyConnector(
     private val volleyQueue: RequestQueue,
-    private val authToken: String
+    private val accessToken: String,
+    private val refreshToken: String
 ) : StreamingConnector {
 
-    override fun search(searchInput: String): TrackList {
-        //TODO
-        return TrackList(mutableListOf())
+    override fun search(searchInput: String, searchList: TrackList) {
+
+        //TODO started implementing here, but the draft got me, nothing works -Nils
+        /*val baseUrl = "https://api.spotify.com/v1/search"
+        val urlSearchQuery = URLEncoder.encode(searchInput, "UTF-8")
+
+        val url = "$baseUrl?q=$urlSearchQuery&type=track&limit=20"
+
+        val stringRequest: StringRequest = object : StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                Log.i("RES", response.toString())
+                searchTracksCallback(JSONObject(response), searchList)
+            },
+            { error ->
+                Log.e("VOLLEY", "Spotify Request Error: ${String(error.networkResponse.data)}")
+            }) {
+            override fun getHeaders(): Map<String, String> {
+                var params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = "Bearer $accessToken"
+                return params
+            }
+        }
+
+        volleyQueue.add(stringRequest)*/
+    }
+
+    private fun searchTracksCallback(artistsJsonObject: JSONObject, searchList: TrackList) {
+
+        //TODO started implementing here, but the draft got me, nothing works -Nils
+        //TODO ATTENTION: Code down here does not compile!!!
+        /*searchList.clear()
+        val trackList = mutableListOf<Track>()
+        val tracksJsonList = artistsJsonObject.getJSONObject("tracks").getJSONArray("items")
+        for(index in 0 until tracksJsonList.length()){
+            val trackJson = tracksJsonList.getJSONObject(index)
+            val trackId = trackJson.getString("id")
+            val trackName = trackJson.getString("name")
+            val trackArtistsJsonList = trackJson.getJSONArray("artists")
+            val artistNames = mutableListOf<String>()
+            for(artistIndex in 0 until trackArtistsJsonList.length()){
+                artistNames.add(trackArtistsJsonList.getJSONObject(artistIndex).getString("name"))
+            }
+
+            val track = Track(trackId, trackName)
+            trackList.add(artistList.getJSONObject(index).getString("name"))
+        }*/
     }
 
 
