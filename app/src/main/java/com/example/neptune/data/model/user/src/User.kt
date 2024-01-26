26a20d34dabba54650cc2f445ba28d6bfa7e3e91
Martin <martinxscheueuermann@online.de>
2024-01-26 12:48:53 +0100

    Added base functionality of user, Everything with need for backendConnection isn´t yet implemented

---
+++ app/src/main/java/com/example/neptune/data/model/user/src/User.kt
@@ -0,0 +1,42 @@
+package com.example.neptune.data.model.user.src
+
+import com.example.neptune.data.model.session.Session
+import com.example.neptune.data.model.track.src.Track
+import com.example.neptune.data.model.track.src.TrackList
+import com.example.neptune.data.model.track.src.VoteList
+
+open class User(val session: Session){
+    private val voteList: VoteList = VoteList()
+    private val blackList: TrackList = TrackList()
+    private val cooldownList: TrackList = TrackList()
+
+
+    fun addTrackToVoteList(track : Track){
+        voteList.addTrack(track)
+    }
+
+    fun upvoteTrack(index: Int){
+        voteList.upvoteTrack(index)
+    }
+    open fun search(input: String): TrackList {
+        var foundTracks = voteList.search(input)
+        foundTracks = filterSearchResults(foundTracks)
+        return TrackList(foundTracks)
+    }
+    protected fun filterSearchResults(searchResult: MutableList<Track>): MutableList<Track>{
+        val output: MutableList<Track> = ArrayList<Track>()
+        for (track in searchResult){
+            if (!blackList.containsTrack(track)&& !cooldownList.containsTrack(track)&&
+                session.validateTrack(track)){
+                output.add(track)
+            }
+        }
+        return output
+
+    }
+    open fun leaveSession(){
+
+    }
+
+
+}
