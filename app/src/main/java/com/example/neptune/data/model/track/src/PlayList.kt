6e4995b0ebb48fd862dcad3596b0cd4d64ec0ada
Martin <martinxscheueuermann@online.de>
2024-01-26 12:49:35 +0100

    Implemented search in PlayList

--- app/src/main/java/com/example/neptune/data/model/track/src/PlayList.kt
+++ app/src/main/java/com/example/neptune/data/model/track/src/PlayList.kt
@@ -2,8 +2,14 @@ package com.example.neptune.data.model.track.src
 
 open class PlayList(tracks: MutableList<Track>): TrackList(tracks) {
     constructor():this(ArrayList<Track>()){}
-    fun search(input :String): TrackList {
-        //Todo implement search algorithm
-        return TrackList(ArrayList());
+    fun search(input :String): MutableList<Track> {
+        val output =  ArrayList<Track>()
+        for( track in tracks){
+            if(track.name.contains(input)){
+                output.add(track)
+            }
+        }
+        tracks.sortBy { track -> track.name.length }
+        return output;
     }
 }
