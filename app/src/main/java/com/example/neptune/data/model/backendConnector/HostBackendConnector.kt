703e1c40db4cc0da761873c75cc2af3ee8e47650
Martin <martinxscheueuermann@online.de>
2024-01-30 11:43:38 +0100

    Fixed Usertest

--- app/src/main/java/com/example/neptune/data/model/backendConnector/HostBackendConnector.kt
+++ app/src/main/java/com/example/neptune/data/model/backendConnector/HostBackendConnector.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import org.json.JSONArray
 import org.json.JSONObject
 
 class HostBackendConnector(
--- app/src/main/java/com/example/neptune/data/model/user/test/UserTest.kt
+++ app/src/main/java/com/example/neptune/data/model/user/test/UserTest.kt
@@ -1,17 +1,18 @@
 package com.example.neptune.data.model.user.test
-import android.util.Log
+import com.android.volley.toolbox.Volley
+import com.example.neptune.NeptuneApp
+import com.example.neptune.data.model.backendConnector.ParticipantBackendConnector
 import com.example.neptune.data.model.session.Session
 import com.example.neptune.data.model.track.test.MockTracks
 import com.example.neptune.data.model.user.src.User
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class UserTest {
-
-
+    val  mockTracks: MockTracks = MockTracks()
+    val backendConnector =ParticipantBackendConnector("234fs", Volley.newRequestQueue(NeptuneApp.context))
     @Test fun searchTest(){
-        val  mockTracks: MockTracks = MockTracks()
-        val user = User(Session(12,23,3))
+
+        val user = User(Session(12,23,3),backendConnector)
         user.voteList = mockTracks.voteList
 
         var result = user.search("B")
