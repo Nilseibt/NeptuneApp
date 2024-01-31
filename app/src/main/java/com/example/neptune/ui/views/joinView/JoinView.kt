0d42b1e114030c00e90290dd905dc467af91040a
simonschtz <78378417+simonschtz@users.noreply.github.com>
2024-01-31 11:40:22 +0100

    jonView: added arrow icon

--- app/src/main/java/com/example/neptune/ui/views/joinView/JoinView.kt
+++ app/src/main/java/com/example/neptune/ui/views/joinView/JoinView.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
