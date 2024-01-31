77a805da707b8781acfaa899f93a835947ff384f
simonschtz <78378417+simonschtz@users.noreply.github.com>
2024-01-31 11:40:04 +0100

    jonView: added arrow icon

--- app/src/main/java/com/example/neptune/ui/views/joinView/JoinView.kt
+++ app/src/main/java/com/example/neptune/ui/views/joinView/JoinView.kt
@@ -10,13 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -62,7 +66,7 @@ fun JoinView(navController: NavController) {
                 enabled = joinViewModel.isCodeInputFormValid()
             )
             {
-                Text(text = "->")
+                Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
             }
         }
         Spacer(modifier = Modifier.height(32.dp))
