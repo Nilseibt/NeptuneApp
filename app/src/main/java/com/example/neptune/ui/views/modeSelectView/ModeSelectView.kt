432ab342f891c4d4162dcba4021982d75ca682af
simonschtz <78378417+simonschtz@users.noreply.github.com>
2024-02-01 18:28:23 +0100

    modeSelectView: Rearranged UI Elements

--- app/src/main/java/com/example/neptune/ui/views/modeSelectView/ModeSelectView.kt
+++ app/src/main/java/com/example/neptune/ui/views/modeSelectView/ModeSelectView.kt
@@ -2,13 +2,25 @@ package com.example.neptune.ui.views.modeSelectView
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -31,44 +43,106 @@ fun ModeSelectView(navController: NavController) {
         modeSelectViewModel.onBack(navController)
     }
 
-    Column() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-        val generalModifier = Modifier
-        if(modeSelectViewModel.isModeSelected(SessionType.GENERAL)){
-            generalModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
-        }
-        Button(onClick = { modeSelectViewModel.onSelectMode(SessionType.GENERAL) }, modifier = generalModifier){
-            Text(text = "General Mode")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                val generalModifier = Modifier
+                    .weight(1f)
+                if (modeSelectViewModel.isModeSelected(SessionType.GENERAL)) {
+                    generalModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
+                }
+                Button(
+                    onClick = { modeSelectViewModel.onSelectMode(SessionType.GENERAL) },
+                    modifier = generalModifier
+                ) {
+                    Text(text = "General Mode")
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                val artistModifier = Modifier
+                    .weight(1f)
+                if (modeSelectViewModel.isModeSelected(SessionType.ARTIST)) {
+                    artistModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
+                }
+                Button(
+                    onClick = { modeSelectViewModel.onSelectMode(SessionType.ARTIST) },
+                    modifier = artistModifier
+                ) {
+                    Text(text = "Artist Mode")
+                }
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                val genreModifier = Modifier
+                    .weight(1f)
+                if (modeSelectViewModel.isModeSelected(SessionType.GENRE)) {
+                    genreModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
+                }
+                Button(
+                    onClick = { modeSelectViewModel.onSelectMode(SessionType.GENRE) },
+                    modifier = genreModifier
+                ) {
+                    Text(text = "Genre Mode")
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                val playlistModifier = Modifier
+                    .weight(1f)
+                if (modeSelectViewModel.isModeSelected(SessionType.PLAYLIST)) {
+                    playlistModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
+                }
+                Button(
+                    onClick = { modeSelectViewModel.onSelectMode(SessionType.PLAYLIST) },
+                    modifier = playlistModifier
+                ) {
+                    Text(text = "Playlist Mode")
+                }
+            }
+
         }
 
-        val artistModifier = Modifier
-        if(modeSelectViewModel.isModeSelected(SessionType.ARTIST)){
-            artistModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
-        }
-        Button(onClick = { modeSelectViewModel.onSelectMode(SessionType.ARTIST) }, modifier = artistModifier){
-            Text(text = "Artist Mode")
-        }
-
-        val genreModifier = Modifier
-        if(modeSelectViewModel.isModeSelected(SessionType.GENRE)){
-            genreModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
-        }
-        Button(onClick = { modeSelectViewModel.onSelectMode(SessionType.GENRE) }, modifier = genreModifier){
-            Text(text = "Genre Mode")
-        }
-
-        val playlistModifier = Modifier
-        if(modeSelectViewModel.isModeSelected(SessionType.PLAYLIST)){
-            playlistModifier.border(5.dp, Color.Black, RoundedCornerShape(50))
-        }
-        Button(onClick = { modeSelectViewModel.onSelectMode(SessionType.PLAYLIST) }, modifier = playlistModifier){
-            Text(text = "Playlist Mode")
-        }
-
-        Text(text = modeSelectViewModel.getSelectedModeDescription())
 
 
-        Button(onClick = { modeSelectViewModel.onConfirmMode(navController) }){
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Box(
+        modifier = Modifier
+            //.border(1.dp, Color.Black, RoundedCornerShape(1))
+            .padding(8.dp)
+            .size(200.dp, 50.dp)
+            .fillMaxWidth()
+        ) {
+            Text(
+                text = modeSelectViewModel.getSelectedModeDescription(),
+                modifier = Modifier
+                    .fillMaxSize(),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(onClick = { modeSelectViewModel.onConfirmMode(navController) }
+        ) {
             Text(text = "Modus Bestätigen")
         }
     }
