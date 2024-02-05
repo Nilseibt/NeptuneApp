4125dd188df54dcf7298bc92a47b3409f484759b
hash-shark <schneider-stephan1@web.de>
2024-02-05 16:46:04 +0100

    Info and Stats View Update

--- app/src/main/java/com/example/neptune/ui/views/infoView/InfoView.kt
+++ app/src/main/java/com/example/neptune/ui/views/infoView/InfoView.kt
@@ -3,9 +3,7 @@ package com.example.neptune.ui.views.infoView
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +12,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.neptune.NeptuneApp
-import com.example.neptune.ui.commons.SessionInfoBar
 import com.example.neptune.ui.commons.TopBar
 import com.example.neptune.ui.views.util.viewModelFactory
 
@@ -29,17 +26,13 @@ fun InfoView(navController: NavController) {
         }
     )
 
-    Column {
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    ) {
 
-        Button(onClick = { infoViewModel.onBack(navController) }) {
-            Text(text = "Info schließen (Icon)")
-        }
-
-        Text(text = "Session Infos (TopBar Beschriftung)")
-
-        Button(onClick = { infoViewModel.onOpenStats(navController) }) {
-            Text(text = "Statistiken öffnen (Icon)")
-        }
+        TopBar(onBack = { infoViewModel.onBack(navController) })
 
         Text(text = "Session Code: " + infoViewModel.getSessionCode())
 
@@ -86,3 +79,40 @@ fun InfoPreview() {
     }
 
 }
+
+/*
+Column {
+
+        Button(onClick = { infoViewModel.onBack(navController) }) {
+            Text(text = "Info schließen (Icon)")
+        }
+
+        Text(text = "Session Infos (TopBar Beschriftung)")
+
+        Button(onClick = { infoViewModel.onOpenStats(navController) }) {
+            Text(text = "Statistiken öffnen (Icon)")
+        }
+
+        Text(text = "Session Code: " + infoViewModel.getSessionCode())
+
+        Text(text = infoViewModel.getModeName())
+
+        if(infoViewModel.isArtistMode()){
+            Text(text = "Artists der Session:")
+            infoViewModel.getAllArtists().forEach {
+                Text(text = it)
+            }
+        }
+
+        if(infoViewModel.isGenreMode()){
+            Text(text = "Genres der Session:")
+            infoViewModel.getAllGenres().forEach {
+                Text(text = it)
+            }
+        }
+
+        Text(text = "Hier vermutlich kein Beitrittscode, denn der geht mir am Arsch vorbei")
+
+        Text(text = infoViewModel.getShareLink())
+    }
+ */
--- app/src/main/java/com/example/neptune/ui/views/statsView/StatsView.kt
+++ app/src/main/java/com/example/neptune/ui/views/statsView/StatsView.kt
@@ -1,17 +1,19 @@
 package com.example.neptune.ui.views.statsView
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.neptune.NeptuneApp
 import com.example.neptune.ui.commons.TopBar
 import com.example.neptune.ui.views.util.viewModelFactory
-import com.example.neptune.ui.views.voteView.VoteViewModel
 
 @Composable
 fun StatsView(navController: NavController) {
@@ -26,15 +28,7 @@ fun StatsView(navController: NavController) {
 
     Column {
 
-        Button(onClick = { statsViewModel.onOpenInfo(navController) }) {
-            Text(text = "Info öffnen (Icon)")
-        }
-
-        Text(text = "Session Stats (TopBar Beschriftung)")
-
-        Button(onClick = { statsViewModel.onBack(navController) }) {
-            Text(text = "Statistiken öffnen (Icon)")
-        }
+        TopBar(onBack = { statsViewModel.onBack(navController) })
 
         Text(text = "Track mit meisten Upvotes: " + statsViewModel.mostUpvotedTrack.value)
         if(statsViewModel.isGenreSession()) {
@@ -57,6 +51,39 @@ fun StatsView(navController: NavController) {
 @Composable
 fun StatsPreview() {
 
-    TopBar {}
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    ) {
+
+        TopBar {}
+
+    }
 
 }
+
+/*
+Column {
+
+        Button(onClick = { statsViewModel.onOpenInfo(navController) }) {
+            Text(text = "Info öffnen (Icon)")
+        }
+
+        Text(text = "Session Stats (TopBar Beschriftung)")
+
+        Button(onClick = { statsViewModel.onBack(navController) }) {
+            Text(text = "Statistiken öffnen (Icon)")
+        }
+
+        Text(text = "Track mit meisten Upvotes: " + statsViewModel.mostUpvotedTrack.value)
+        if(statsViewModel.isGenreSession()) {
+            Text(text = "Genre mit meisten Upvotes: " + statsViewModel.mostUpvotedGenre.value)
+        }
+        Text(text = "Artist mit meisten Upvotes: " + statsViewModel.mostUpvotedArtist.value)
+        Text(text = "Gespielte Tracks: " + statsViewModel.totalPlayedTracks.value)
+        Text(text = "Session-Dauer: " + statsViewModel.sessionDuration.value)
+        Text(text = "Teilnehmer-Anzahl: " + statsViewModel.totalParticipants.value)
+        Text(text = "Upvote-Anzahl: " + statsViewModel.totalUpvotes.value)
+    }
+ */
