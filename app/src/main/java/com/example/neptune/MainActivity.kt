db91eea8c6c59e7451c04e23781a86667a1836a8
Martin <martinxscheueuermann@online.de>
2024-02-06 14:25:22 +0100

    Tryed to add links, kindd

--- app/src/main/java/com/example/neptune/MainActivity.kt
+++ app/src/main/java/com/example/neptune/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.neptune.ui.NeptuneNavGraph
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             NeptuneNavGraph(this)
         }
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
     }
 }
