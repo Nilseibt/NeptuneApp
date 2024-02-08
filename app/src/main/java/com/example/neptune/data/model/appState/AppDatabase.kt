7541570632006d1d47035da93a6db8e43797ef66
Nilseibt <151017758+Nilseibt@users.noreply.github.com>
2024-02-08 22:38:49 +0100

    Added Kdoc to the AppState package and the BackendConnector package

--- app/src/main/java/com/example/neptune/data/model/appState/AppDatabase.kt
+++ app/src/main/java/com/example/neptune/data/model/appState/AppDatabase.kt
@@ -4,29 +4,45 @@ import com.example.neptune.data.room.app.AppData
 import com.example.neptune.data.room.app.AppDataDao
 
 
+/**
+ * Wrapper for the database for storing application state-related data, which is only the deviceId.
+ * @property appDataDao The Data Access Object (DAO) for accessing AppData entities in the actual room database.
+ */
 class AppDatabase(
     private val appDataDao: AppDataDao
 ) {
 
 
+    /**
+     * Checks if the device ID exists in the database.
+     * @return true if the device ID exists, false otherwise.
+     */
     suspend fun hasDeviceId(): Boolean {
         return appDataDao.entryCount() != 0
     }
 
+    /**
+     * Sets the device ID in the database if it does not already exist.
+     * @param deviceId The device ID to be set.
+     * @throws Exception if the device ID is already set.
+     */
     suspend fun setDeviceId(deviceId: String) {
         if (!hasDeviceId()) {
             appDataDao.upsert(AppData(0, deviceId))
         } else {
-            //TODO ist das sinnvoll???
             throw Exception("Device Id is already set, cannot be reset")
         }
     }
 
+    /**
+     * Retrieves the device ID from the database.
+     * @return The device ID.
+     * @throws Exception if the device ID is not set.
+     */
     suspend fun getDeviceId(): String {
         if (hasDeviceId()) {
             return appDataDao.getDeviceId()
         } else {
-            //TODO ist das sinnvoll???
             throw Exception("Device Id is not set, but must be already set")
         }
     }
--- app/src/main/java/com/example/neptune/data/model/backendConnector/HostBackendConnector.kt
+++ app/src/main/java/com/example/neptune/data/model/backendConnector/HostBackendConnector.kt
@@ -5,12 +5,25 @@ import com.example.neptune.data.model.track.Track
 import org.json.JSONArray
 import org.json.JSONObject
 
+/**
+ * Connector class for host-specific backend interactions, extending the base [BackendConnector].
+ * @param deviceId The unique identifier of this host device.
+ * @param volleyQueue The Volley request queue for network communication.
+ */
 class HostBackendConnector(
     private val deviceId: String,
     private val volleyQueue: RequestQueue
 ) : BackendConnector(deviceId, volleyQueue) {
 
 
+    /**
+     * Creates a new session with the specified session parameters.
+     * @param mode The mode of the session ("General", "Artist", "Genre", "Playlist").
+     * @param cooldownTimer The cooldown timer duration for played tracks in the session.
+     * @param artists The list of artists associated with the session, should be empty if not artist session.
+     * @param genres The list of genres associated with the session.
+     * @param callback Callback function to handle the response containing session ID and timestamp.
+     */
     fun createNewSession(
         mode: String,
         cooldownTimer: Int,
@@ -42,7 +55,9 @@ class HostBackendConnector(
 
 
 
-
+    /**
+     * Deletes the current session, causes participants to be removed from the deleted session.
+     */
     fun deleteSession() {
         val postData = JSONObject()
         postData.put("hostDeviceID", deviceId)
@@ -51,6 +66,10 @@ class HostBackendConnector(
     }
 
 
+    /**
+     * Indicates that a track has been played within the session, activating the cooldown.
+     * @param track The track that has been played.
+     */
     fun playedTrack(track: Track) {
         val postData = JSONObject()
         postData.put("hostDeviceID", deviceId)
@@ -60,6 +79,11 @@ class HostBackendConnector(
     }
 
 
+    /**
+     * Sets the blocked status of a track within the session.
+     * @param track The track to set the blocked status for.
+     * @param blocked Boolean value indicating whether the track is blocked.
+     */
     fun setBlockTrack(track: Track, blocked: Boolean) {
         val postData = JSONObject()
         postData.put("hostDeviceID", deviceId)
